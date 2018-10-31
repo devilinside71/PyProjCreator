58fdf070f2187ee558a40c908d98b972e3b5f456
devilinside71 <devilinside71@users.noreply.github.com>
2018-10-31 11:25:44 +0100

    Init2

--- pyprojcreator.py
+++ pyprojcreator.py
@@ -9,8 +9,9 @@ import sys
 import argparse
 import os
 import errno
+# from tkinter import *
 
-logger = logging.getLogger('program')
+logger = logging.getLogger('pyprojcreator')
 # set level for file handling (NOTSET>DEBUG>INFO>WARNING>ERROR>CRITICAL)
 logger.setLevel(logging.DEBUG)
 
@@ -40,8 +41,12 @@ def parse_arguments():
     @return arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-pf', '--projectfolder',
+    parser.add_argument('-f', '--folder',
                         help='target folder of the project')
+    parser.add_argument('-n', '--name',
+                        help='name of the project')
+    parser.add_argument('-d', '--description',
+                        help='description of the project')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='increase output verbosity')
     return parser.parse_args()
@@ -52,35 +57,76 @@ def execute_program():
     """
 
     args = parse_arguments()
-    create_folder(args.projectfolder, args.verbose)
+    res_name = ""
+    res_desc = ""
+    res_folder = create_folder(args.folder)
+    if res_folder == True:
+        res_name = get_projname(args.name)
+        res_desc = get_projdesc(args.description)
+        if res_name == False or res_desc == False:
+            logger.debug("Start GUI to add project name and/or description")
+            # https://www.python-course.eu/tkinter_entry_widgets.php
+        else:
+            logger.debug(
+                "No GUI needed to add project name and/or description")
+            logger.debug("Project name: '" + args.name)
+            logger.debug("Project description: '" + args.description)
 
 
-def create_folder(folder_name, verbosity):
+def create_folder(folder_name):
     """Create folder for new project.
 
     Arguments:
         folder_name {str} -- target folder name
 
+    Returns:
+        bool -- folder created succesfuly
     """
-    if verbosity:
-        print("Target folder: " + folder_name)
-    else:
-        print(folder_name)
+    ret = False
+    logger.debug("Target folder: " + folder_name)
     # Check if folder exists
     folder_exists = os.path.isdir(folder_name)
     if folder_exists:
-        if verbosity:
-            print("Folder '" + folder_name + "' already exists")
-        else:
-            print(str(folder_exists))
+        logger.debug("Folder '" + folder_name + "' already exists")
     else:
        # create folder
         try:
             os.makedirs(folder_name)
-            # code here
+            logger.debug("Folder '" + folder_name + "' created succesfuly")
+            ret = True
         except OSError as e:
+            ret = False
             if e.errno != errno.EEXIST:
+                logger.debug('Cannot create folder ' + folder_name)
+                logger.debug('Exit program')
                 raise
+    return ret
+
+
+def get_projname(proj_name):
+    """Get project name
+
+    Returns:
+        str -- project name
+    """
+    ret = True
+    if proj_name == None:
+        ret = False
+    logger.debug("New project name: '" + str(proj_name) + "'")
+    return ret
+
+
+def get_projdesc(proj_desc):
+    """Get project description
+
+    Returns:
+        str -- project description
+    """
+    ret = True
+    if proj_desc == None:
+        ret = False
+    logger.debug("New project description: '" + str(proj_desc) + "'")
+    return ret
 
 
 if __name__ == '__main__':
