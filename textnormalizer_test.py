2d936c6fd74f8264e509d938db74cd478d2e8fbb
devilinside71 <devilinside71@users.noreply.github.com>
2018-11-06 15:22:18 +0100

    Unittest bugfix

--- textnormalizer_test.py
+++ textnormalizer_test.py
@@ -16,14 +16,18 @@ class testFunctions(unittest.TestCase):
     def setUp(self):
         self.test_str_01 = 'Árvíztűrő tükörfúrógép'
         self.test_res_01 = 'Arvizturo_tukorfurogep'
+        self.test_str_02 = 'Árvíztűrő tükörfúrógép'
+        self.test_res_02 = 'Arvizturo tukorfurogep'
 
-    def test_sample_function(self):
-        """Test01
+    def test_get_normalized_name(self):
+        """Test for get_normalized_name function.
         """
 
         test_class = textnormalizer.TextNormalizer()
         test_class.string_to_normalize = self.test_str_01
         self.assertEqual(test_class.get_normalized_name(), self.test_res_01)
+        test_class.string_to_normalize = self.test_str_02
+        self.assertEqual(test_class.get_normalized_name(), self.test_res_02)
 
 
 if __name__ == '__main__':
